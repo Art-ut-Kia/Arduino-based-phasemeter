0f6dcfe92e31d270e9146cad03e5580052ecf183
Jean-Paul PETILLON <Art-ut-Kia@users.noreply.github.com>
2016-07-20 12:53:41 +0200

    Added 2x16 LCD to display phase
    Phase measurement is sent on both serial link and LCD.
    Operation of an LCD is configurable.

--- Phasemeter.ino
+++ Phasemeter.ino
@@ -1,31 +1,40 @@
-/*****************************************************************************************
- * Phasemeter.ino :                                                                      *
- * - Receives two negative pulses of 1ms at a rate of 50Hz on inputs #2 and #3 :         *
- *                           <1><--------19----> (ms)                                    *
- *                   ________   ________________   _____________________                 *
- *            #2             |_|                |_|                                      *
- *                   ______________________   ________________   _______                 *
- *            #3                           |_|                |_|                        *
- *                            <---- d ---->                                              *
- * - Interrupt handlers are attached to these pins, both sensitive to falling edges      *
- * - At each falling edge on input #3, transmits the time "d" elapsed since last falling *
- *   edge on pin #2.                                                                     *
- *                         Author : Jean-Paul PETILLON                                   *
- * ----------------------------------- GNU GPL ----------------------------------------- *
- * This program is free software: you can redistribute it and/or modify it under the     *
- * terms of the GNU General Public License as published by the Free Software Foundation, *
- * either version 3 of the License, or (at your option) any later version.               *
- *                                                                                       *
- * This program is distributed in the hope that it will be useful, but without any       *
- * warranty; without even the implied warranty of merchantability or fitness for a       *
- * particular purpose. See the GNU General Public License for more details.              *
- *                                                                                       *
- * You should have received a copy of the GNU General Public License along with this     *
- * program. If not, see <http://www.gnu.org/licenses/>.                                  *
- *****************************************************************************************/
-//#define selftest
- 
+//---------------------------------------------------------------------------------------//
+// Phasemeter.ino :                                                                      //
+// - Receives two negative pulses of 1ms at a rate of 50Hz on inputs #2 and #3 :         //
+//                           <1><--------19----> (ms)                                    //
+//                   ________   ________________   _____________________                 //
+//            #2             |_|                |_|                                      //
+//                   ______________________   ________________   _______                 //
+//            #3                           |_|                |_|                        //
+//                            <---- d ---->                                              //
+// - Interrupt handlers are attached to these pins, both sensitive to falling edges      //
+// - At each falling edge on input #3, transmits on serial link the time "d" elapsed     //
+//   since last falling edge on pin #2.                                                  //
+//                         Author : Jean-Paul PETILLON                                   //
+//------------------------------------ GNU GPL ------------------------------------------//
+// This program is free software: you can redistribute it and/or modify it under the     //
+// terms of the GNU General Public License as published by the Free Software Foundation, //
+// either version 3 of the License, or (at your option) any later version.               //
+//                                                                                       //
+// This program is distributed in the hope that it will be useful, but without any       //
+// warranty; without even the implied warranty of merchantability or fitness for a       //
+// particular purpose. See the GNU General Public License for more details.              //
+//                                                                                       //
+// You should have received a copy of the GNU General Public License along with this     //
+// program. If not, see <http://www.gnu.org/licenses/>.                                  //
+//---------------------------------------------------------------------------------------//
+
+#include <LiquidCrystal.h>
+
+#define uselcd   // comment or uncomment the line to implement the function
+//#define selftest // comment or uncomment the line to implement the function
+
+#ifdef uselcd  // RS, EN, D4, D5, D6, D7
+LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
+#endif // (def selftest)
+
 volatile long int t0;
+volatile unsigned char phase;
 
 void setup() {
   // shutdown the LED connected to pin #13
@@ -33,9 +42,9 @@ void setup() {
 
 # ifdef selftest
   // for testing purpose: pins #4 & #5 are used to stimulate pins #2 and #3
-  pinMode( 4, OUTPUT); digitalWrite( 4, HIGH);
-  pinMode( 5, OUTPUT); digitalWrite( 5, HIGH);
-# endif
+  pinMode(4, OUTPUT); digitalWrite(4, HIGH);
+  pinMode(5, OUTPUT); digitalWrite(5, HIGH);
+# endif // (def selftest)
 
   // initializes the phase-meter
   Serial.begin(9600);
@@ -43,9 +52,19 @@ void setup() {
   pinMode(3, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), int2, FALLING);
   attachInterrupt(digitalPinToInterrupt(3), int3, FALLING);
+
+  // initializes the LCD
+# ifdef uselcd
+  lcd.begin(16, 2);    // defines the LCD type
+  lcd.setCursor(0, 0); // set the cursor at the begining of the 1st line
+  lcd.print(" phase | Phase:");
+  lcd.setCursor(0, 1); // set the cursor at the begining of the 2nd line
+  lcd.print(" meter |      ms");
+# endif // (def selftest)
 }
 
 void loop() {
+  //-------------------- generates two test signals, on pins #4 & #5 ----------------------
 # ifdef selftest
   // sends a 1ms negative pulse on output #4
   digitalWrite(4, LOW);
@@ -60,7 +79,18 @@ void loop() {
   digitalWrite(5, HIGH);
   // waits for the balance to 20ms (50Hz)
   delay(20 - 2 - DelayMillis);
-# endif
+# endif // (def selftest)
+
+  //--------------------- displays the measurement on the 2 x 16 LCD ----------------------
+# ifdef uselcd
+  float fPhase = 0.128 * phase; // recomputes measurement in millisecond, floating point
+  lcd.setCursor(9, 1); // cursor at the begining of the phase display field
+  if (fPhase<10) lcd.print(" "); // to deal with the variable length of the phase display
+  lcd.print(fPhase, 1);
+  lcd.setCursor(13,1);
+  lcd.print(" ");
+# endif // (def uselcd)
+  //------------------------------------ end of loop() ------------------------------------
 }
 
 void int2() {
@@ -68,13 +98,13 @@ void int2() {
 }
 
 void int3() {
-  // calculates the delay in µs between falling edge on pin #2 a,d falling edge on pin #3:
+  // computes delay (in 128µs quanta) between falling edges on pin #2 and pin #3:
   //                               micros()-t0
-  // And transmits it over the Arduino UART Tx signal
-  
-  // ASCII string terminated by CR/LF (for direct reading, in µs, in hyperterminal)
-//Serial.println(micros()-t0);
+  phase = constrain((micros()-t0)>>7, 0, 255);
 
+  // transmits it over the Arduino UART Tx signal
+  // ASCII string (in ms) terminated by CR/LF (for direct reading in hyperterminal)
+//Serial.println(0.128 * phase);
   // single character binary, [0 ... 255] domain, LSB = 128µs (0 .. 32.64ms)
-  Serial.write(constrain((micros()-t0)>>7, 0, 255));
+  Serial.write(phase);
 }
